674efaef82a16377b6f55ca7b834b8bec257855c
FendiArd <syndicateard@gmail.com>
2024-03-26 20:55:40 +0700

    Trainware Shappire

--- BAB-3 Tutorial Per Modul/JP6 RAIN GAS BTN/BUTTON/BUTTON.ino
+++ BAB-3 Tutorial Per Modul/JP6 RAIN GAS BTN/BUTTON/BUTTON.ino
@@ -4,10 +4,10 @@
 =================== */
 
 // Mendefinisikan pin BUTTON-1 sampai BUTTON-4 pada ESP32
-#define pin_Bt_1 12  // GIOP13 pin connected to button
+#define pin_Bt_1 12  // GIOP12 pin connected to button
 #define pin_Bt_2 13  // GIOP13 pin connected to button
-#define pin_Bt_3 35  // GIOP13 pin connected to button
-#define pin_Bt_4 34  // GIOP13 pin connected to button
+#define pin_Bt_3 35  // GIOP35 pin connected to button
+#define pin_Bt_4 34  // GIOP34 pin connected to button
 
 void setup() {
   // Inisialiasasi Serial, dengan baudrate 115200
@@ -23,10 +23,10 @@ void setup() {
 void loop() {
   
   // Menampilkan hasil pembacaan button di Serial Monitor
-  Serial.print(digitalRead(pin_Bt_1));
-  Serial.print(digitalRead(pin_Bt_2));
-  Serial.print(digitalRead(pin_Bt_3));
-  Serial.println(digitalRead(pin_Bt_4));
+  digitalRead(pin_Bt_1) && Serial.println("Button 1 ditekan");
+  digitalRead(pin_Bt_2) && Serial.println("Button 2 ditekan");
+  digitalRead(pin_Bt_3) && Serial.println("Button 3 ditekan");
+  digitalRead(pin_Bt_4) && Serial.println("Button 4 ditekan");
 
   // Jeda 100 ms
   delay(100);
