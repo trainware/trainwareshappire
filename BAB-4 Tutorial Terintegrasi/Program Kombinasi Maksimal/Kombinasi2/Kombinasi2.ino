513c4672363f2fda5fe18fa61438405bf424b149
FendiArd <syndicateard@gmail.com>
2024-03-27 20:58:31 +0700

    Trainware Shappire

--- BAB-4 Tutorial Terintegrasi/Program Kombinasi Maksimal/Kombinasi2/Kombinasi2.ino
+++ BAB-4 Tutorial Terintegrasi/Program Kombinasi Maksimal/Kombinasi2/Kombinasi2.ino
@@ -17,6 +17,8 @@
 // Mendefinisikan pin motionSensor pada ESP32
 #define pin_PIR 25
 
+unsigned long timer_delay_pir=0;
+
 /*=================================================== ADD LED ===========*/
 // Mendefinisikan pin LED pada ESP32
 #define LED_1   32
@@ -140,31 +142,97 @@ void setup() {
   // Inisialisasi start Servo 
   myservo.attach(ServoPin);
 
+  myservo.write(pos);
+  /*=================================================== OLED ===========*/
+  display.setCursor(0, 10);
+  // Fungsi menampilkan Text
+  display.println("Servo:"); 
+
+  display.setCursor(75, 10);
+  // Fungsi menampilkan Text
+  display.println(0);
+
+  // Fungsi menampilkan Text
+  display.setCursor(0, 35);
+  display.print("Kosong");
+
+  display.display();              
+  delay(15);
 }
 
 void loop() {
+  // Clear display OLED
+  display.clearDisplay();
+
+  /*=================================================== RAIN ===========*/
+  // Membaca nilai ADC dari sensor Air hujan
+  int rainAnalogVal = analogRead(rainAnalog);
+
+  // Membaca nilai digital dari sensor Air hujan
+  int rainDigitalVal = digitalRead(rainDigital);
+
+  // Menampilkan hasil pembacaan melalui serial monitor
+  // Serial.print(rainAnalogVal);
+  // Serial.print("\t");
+  // Serial.println(rainDigitalVal);
+
+  /*=================================================== LCD ===========*/
+  // Mengatur posisi text pada X = 0 ; Y = 0.
+  lcd.setCursor(0, 0);
+  // Text yang ditampilkan
+  lcd.print("Moisture:");
   
+  // Mengatur posisi text pada X = 9 ; Y = 0.
+  lcd.setCursor(10, 0);
+  // Text yang ditampilkan
+  lcd.print(value);
+  lcd.setCursor(14, 0);
+  // Text yang ditampilkan
+  lcd.print("%");
+
+  // Mengatur posisi text pada X = 0 ; Y = 1.
+  lcd.setCursor(0, 1);
+  // Text yang ditampilkan
+  lcd.print("Rain:");
+  
+  // Mengatur posisi text pada X = 6 ; Y = 1.
+  lcd.setCursor(6, 1);
+  // Text yang ditampilkan
+  lcd.print(rainAnalogVal);
+
   /*=================================================== PIR ===========*/
   // fungsi untuk mengambil data pembacaan sensor PIR
   bool state_pir = digitalRead(pin_PIR);
   
   // jika kondisi terbaca = 0, maka serial akan menampilkan 
   // "Motion Tidak Terdeteksi"
-  if (state_pir == 0){
+  if (state_pir == 0 && (millis()-timer_delay_pir) >= 3000){
     Serial.println("Motion Tidak Terdeteksi");  
+    /*=================================================== ADD LED ===========*/
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+    /*=================================================== OLED ===========*/
+    // Fungsi menampilkan Text
+    display.setCursor(0, 35);
+    display.print("Kosong");
   } 
   // jika kondisi terbaca 1, maka serial akan menampilkan 
   // "Motion Terdeteksi"
-  else {
+  else if (state_pir == 1){
     Serial.println("Motion Terdeteksi");
-
+    /*=================================================== ADD LED ===========*/
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, HIGH);
+    /*=================================================== OLED ===========*/
+    // Fungsi menampilkan Text
+    display.setCursor(0, 35);
+    display.print("Ada Orang");
+    timer_delay_pir=millis();
   }
-
-  /*=================================================== ADD LED ===========*/
-  digitalWrite(LED_1, HIGH);
-  digitalWrite(LED_2, HIGH);
-  digitalWrite(LED_3, HIGH);
-  digitalWrite(LED_4, HIGH);
 
   /*=================================================== KEYPAD ===========*/
   // Variable penampung Tombol Keypad yang ditekan
@@ -174,24 +242,40 @@ void loop() {
   // maka Akan ditampilkan tombol yang ditekan melalui Serial Monitor
   if (key) {
     Serial.println(key);
+        
+    if(key=='1'){
+      pos=0;
+    }
+    else if(key=='2'){
+      pos=30;
+    }
+    else if(key=='3'){
+      pos=60;
+    }
+    else if(key=='4'){
+      pos=90;
+    }
+    else if(key=='5'){
+      pos=120;
+    }
+    else if(key=='6'){
+      pos=150;
+    }
+    else if(key=='7'){
+      pos=180;
+    }
+    myservo.write(pos);
   }
+  /*=================================================== OLED ===========*/
+  display.setCursor(0, 10);
+  // Fungsi menampilkan Text
+  display.println("Servo:"); 
 
-  if(key=='1'){
-    digitalWrite(LED_1, LOW);
-    delay(500);
-  }
-  if(key=='2'){
-    digitalWrite(LED_2, LOW);
-    delay(500);
-  }
-  if(key=='3'){
-    digitalWrite(LED_3, LOW);
-    delay(500);
-  }
-  if(key=='4'){
-    digitalWrite(LED_4, LOW);
-    delay(500);
-  }
+  display.setCursor(75, 10);
+  // Fungsi menampilkan Text
+  display.println(pos);             
+  delay(15);
+  display.display(); 
 
   /*=================================================== SOIL SENSE ===========*/
   // Fungsi mengambil data Analog Soil Sensor dengan ADC
@@ -199,78 +283,15 @@ void loop() {
 
   // Rescale data ADC dari Soil Sensor
   // Rescale nilai 550-0 menjadi 0-100
-  value = map(value_soil,225,0,0,100);
-  Serial.print("Moisture : ");
+  value = map(value_soil,4095,0,0,100);
+  Serial.print("Soil Raw Data : ");
   Serial.print(value_soil);
+  Serial.print("| Moisture : ");
+  Serial.print(value);
   Serial.println("%");
-  Serial.print("Hasil D0 :");
+  Serial.print("Hasil D0 Soil :");
   Serial.println(digitalRead(soilPinDigital));
 
-  /*=================================================== LCD ===========*/
-  // Mengatur posisi text pada X = 0 ; Y = 0.
-  lcd.setCursor(0, 0);
-  // Text yang ditampilkan
-  lcd.print("Moisture:");
-  
-  // Mengatur posisi text pada X = 9 ; Y = 0.
-  lcd.setCursor(11, 0);
-  // Text yang ditampilkan
-  lcd.print(value);
-   
-  // Mengatur posisi text pada X = 0 ; Y = 1.
-  lcd.setCursor(0,1);
-  // Text yang ditampilkan
-  lcd.print("Keypad:");
-  
-  // Mengatur posisi text pada X = 9 ; Y = 1.
-  lcd.setCursor(11,1);
-  // Text yang ditampilkan
-  lcd.print(key);
-  
-  
-
-  /*=================================================== RAIN ===========*/
-   // Membaca nilai ADC dari sensor Air hujan
-  int rainAnalogVal = analogRead(rainAnalog);
-
-  // Membaca nilai digital dari sensor Air hujan
-  int rainDigitalVal = digitalRead(rainDigital);
-
-  // Menampilkan hasil pembacaan melalui serial monitor
-  Serial.print(rainAnalogVal);
-  Serial.print("\t");
-  Serial.println(rainDigitalVal);
-
-  /*=================================================== SERVO ===========*/
-  // Fungsi gerakan servo dari 0 derajat hingga 180 derajat
-  for (pos = 0; pos <= 180; pos += 1) {
-    myservo.write(pos);
-    /*=================================================== OLED ===========*/
-    display.clearDisplay();
-    display.setCursor(0, 10);
-    // Fungsi menampilkan Text
-    display.println("Servo"); 
-
-    display.setCursor(0, 35);
-    // Fungsi menampilkan Text
-    display.println(pos);
-    display.display();              
-    delay(15);
-  }
-  // Fungsi gerakan servo dari 180 derajat hingga 0 derajat
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    myservo.write(pos);  
-    /*=================================================== OLED ===========*/
-    display.clearDisplay();
-    display.setCursor(0, 10);
-    // Fungsi menampilkan Text
-    display.println("Servo"); 
-
-    display.setCursor(0, 35);
-    // Fungsi menampilkan Text
-    display.println(pos);
-    display.display();              
-    delay(15);                       
-  }
+  delay(10);
   lcd.clear(); 
 }
